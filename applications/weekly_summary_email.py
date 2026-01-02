08252d61c14d90cc8d4898b031513c4dfb38c3a8
mikel0112 <mikelcampo0112@gmail.com>
2026-01-02 09:20:40 +0100

    email send date changes to mondays

--- applications/weekly_summary_email.py
+++ applications/weekly_summary_email.py
@@ -229,7 +229,7 @@ def ejecutar_proceso_completo():
         logger.info("INICIANDO ENVÍO DE EMAILS")
         logger.info("="*60)
         
-        date = datetime.datetime.today() - datetime.timedelta(days=6) 
+        date = datetime.datetime.today() - datetime.timedelta(days=7) 
         date_string = date.strftime("%Y-%m-%d")
         logger.info(f"Fecha de reporte: {date_string}")
         
